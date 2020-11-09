a40f3f8f0026bfa2ad69d141cc6ca75abf5a3cb8
nikhil12-cmd <73543642+nikhil12-cmd@users.noreply.github.com>
2020-11-09 08:10:05 +0530

    Add files via upload

---
+++ untitled9.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 08:08:06 2020
+
+@author: nikhilchaudhary
+"""
+Flask==0.11.1
+passlib==1.7.0
+SQLAlchemy==1.1.5
+Werkzeug==0.11.15
+gunicorn==19.0.0
+gevent==1.2.1
